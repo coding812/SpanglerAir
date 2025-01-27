551b36e198a69fca2bcb5408294e3857d0e86d4c
Steve <coding812@gmail.com>
2025-01-27 17:03:52 -0500

    Added new background image, transition opacity of nav bar with scrolling

--- src/app/About-Us/page.js
+++ src/app/About-Us/page.js
@@ -16,30 +16,22 @@ export default function Team() {
   }, []);
 
   return (
-    <div className="relative overflow-hidden bg-slate-400">
+    <div className="min-h-full bg-custom-blue">
       <div
-        className="absolute inset-0 bg-no-repeat bg-fixed parallax-bg bg-center md:bg-top md:bg-cover"
-        style={{
-          backgroundImage: "url('/portrait-family.jpg')",
-          backgroundSize: "cover",
-          backgroundAttachment: "fixed",
-          // backgroundPosition: "center", // Center on mobile
-          opacity: opacity,
-          transition: 'opacity 0.3s'
+        className="absolute z-1 inset-0 bg-no-repeat bg-fixed bg-cover parallax-bg bg-center md:bg-top md:bg-cover"
+        style={{backgroundImage: "url('/family-portrait.jpg')"
+        , opacity: opacity
         }}
       >
       </div>
-      <div className="relative mt-16 p-4"></div>
-      <div className="relative mt-16 p-4">
-        <div className='md:text-4xl text-lg text-neutral-950 md:m-16 ' >
-          <div className='inline-block md:w-1/3 w-full rounded-xl bg-slate-400/40 p-6 '>
-            <p className='indent-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div className="relative md:mt-12 mt-64 text-slate-100 p-4 ">
+        <div className='md:text-4xl text-lg md:m-16 ' >
+          <div className='inline-block md:w-1/3 w-full rounded-xl bg-custom-blue/55 p-6 '>
+            <p className='indent-20 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <p className='indent-20 p-4'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <p className='indent-20 p-4'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
           
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          <p className='indent-20 p-4'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
--- src/app/Shared/navbar.js
+++ src/app/Shared/navbar.js
@@ -1,77 +1,71 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [opacity, setOpacity] = useState(0.85);
+
+  const openMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const newOpacity = Math.min(0.85 + scrollY / 500, 1);
+      setOpacity(newOpacity);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="min-h-full bg-slate-400">
-      <nav className="bg-custom-blue mt-6">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-10">
-          <div className="flex h-16 items-center justify-between">
+    <div className="">
+      <nav className="bg-custom-blue  mt-6 md:mt-8 fixed top-0 left-0 w-full z-50 transition-opacity duration-700" style={{opacity: opacity}}>
+        <div className="mx-auto max-w-8xl px-4 md:px-20">
+          <div className="flex h-12 md:h-14 items-center justify-between">
             <div className="flex items-center">
-              <div className="shrink-0">
-                <img className="h-24 w-auto lg:h-36 lg:mt-4  " src="snow_flame.png" alt="Your Company" />
+              <div className="shrink-0 ">
+                <img className="h-24 w-auto lg:h-36 lg:mt-4" style={{ opacity: 1 }} src="snow_flame.png" alt="Your Company" />
               </div>
               <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                <div className="flex items-baseline space-x-4">
                   <a href="/" className={`rounded-md px-3 py-2 text-3xl font-serif font-bold ${pathname === "/" ? "bg-gray-900 text-white" : "text-white hover:bg-gray-700 hover:text-white"}`} aria-current="page">Home</a>
                   <a href="About-Us" className={`rounded-md px-3 py-2 text-3xl font-serif font-bold ${pathname === "/Team" ? "bg-gray-900 text-white" : "text-white hover:bg-gray-700 hover:text-white"}`}>About Us</a>
-                  {/* <a href="Projects" className={`rounded-md px-3 py-2 text-3xl font-serif font-bold ${pathname === "/Projects" ? "bg-gray-900 text-white" : "text-white hover:bg-gray-700 hover:text-white"}`}>Projects</a> */}
-                  {/* <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a> */}
-                  {/* <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a> */}
                 </div>
               </div>
             </div>
-            <div className="hidden md:block">
-              
-            </div>
-            {/* <!-- Mobile menu button --> */}
-            <div className="-mr-2 flex md:hidden">
-              <button type="button" className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false" onClick={openMenu}>
-                <span className="absolute -inset-0.5"></span>
+            <div className="mr-2 flex md:hidden">
+              <button type="button" className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded={menuOpen} onClick={openMenu}>
                 <span className="sr-only">Open main menu</span>
-                {/* <!-- Menu open: "hidden", Menu closed: "block" --> */}
-                <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                <svg className={`${menuOpen ? "hidden" : "block"} h-6 w-6`} fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
-                {/* <!-- Menu open: "block", Menu closed: "hidden" --> */}
-                <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                <svg className={`${menuOpen ? "block" : "hidden"} h-6 w-6`} fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
-            {/* <!-- Mobile menu button --> */}
           </div>
         </div>
       </nav>
 
-      {/* <!-- Mobile menu, show/hide based on menu state --> */}
-      <div className="md:hidden bg-custom-blue " id="mobile-menu" style={{ display: 'none' }}>
+      {/* Mobile menu, show/hide based on menu state */}
+      <div className={`fixed mt-2 p-3 md:hidden bg-custom-blue  top-16 left-0 w-full z-40 transition-opacity duration-700 ${menuOpen ? "block" : "hidden"}`} style={{opacity: opacity}} id="mobile-menu">
         <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-          <a href="/" className="block rounded-md px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-          <a href="About-Us" className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700 hover:text-white">About Us</a>
-          {/* <a href="Projects" className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700 hover:text-white">Projects</a> */}
-          {/* <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a> */}
-          {/* <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a> */}
+          <a href="/" className="block rounded-md px-3 py-2 font-medium text-lg  text-white" aria-current="page">Home</a>
+          <a href="About-Us" className="block rounded-md px-3 py-2 font-medium text-lg text-white hover:bg-gray-700 hover:text-white">About Us</a>
         </div>
       </div>
 
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          {/* <!-- Your content --> */}
+          {/* Your content */}
         </div>
       </main>
     </div>
   );
-
-  // function to open and close menu on mobile
-  function openMenu() {
-    var menu = document.getElementById("mobile-menu");
-    if (menu.style.display === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
-  }
 }
